4ad1c2b347d6ee88ec57cd76304bca03a21749d1
ItsRaminTime <rahmari@stanford.edu>
2017-05-14 23:52:43 -0700

    M

---
+++ Code/sentiment_analyser.py
@@ -0,0 +1,48 @@
+import nltk.classify.util
+from nltk.classify import NaiveBayesClassifier
+from nltk.corpus import movie_reviews
+import csv
+import re
+
+ 
+def word_feats(self, words):
+	return dict([(word, True) for word in words])
+
+def train_emotion_analyzer(self):
+ 
+	negids = movie_reviews.fileids('neg')
+	posids = movie_reviews.fileids('pos')
+	 
+	negfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'neg') for f in negids]
+	posfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'pos') for f in posids]
+	 
+	negcutoff = len(negfeats)*3/4
+	poscutoff = len(posfeats)*3/4
+	 
+	trainfeats = negfeats[:negcutoff] + posfeats[:poscutoff]
+	testfeats = negfeats[negcutoff:] + posfeats[poscutoff:]
+	print 'train on %d instances, test on %d instances' % (len(trainfeats), len(testfeats))
+	 
+	classifier = NaiveBayesClassifier.train(trainfeats)
+	print 'accuracy:', nltk.classify.util.accuracy(classifier, testfeats)
+	classifier.show_most_informative_features()
+
+
+
+file_name = "../Speed Dating/anonspeeddate/anon_102-122.txt"
+with open(file_name) as f:
+    reader = csv.reader(f, delimiter="\t")
+    list_reader = list(reader)
+
+count = 0
+for line in list_reader:
+	print(line)
+	line = re.sub(' ', '\t', line)
+	content[count] = line
+	count = count + 1
+
+print(content)
+print(content[0])
+print(content[1])
+print(content[3][0])
+
