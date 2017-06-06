3472725e1029cf0d5d7f2fc68c29af9872a4ae24
Unknown <cfsalguero93@gmail.com>
2017-06-05 19:02:44 -0700

    Beginning implementation of prediction streams utilizing IBM Watson Utility

--- prediction_stream.py
+++ prediction_stream.py
@@ -4,3 +4,14 @@ import sys
 import os
 import csv
 import prosody_reaper
+
+
+class PredictionStreamer(object):
+    """docstring for PredictionStreamer"""
+    def __init__(self):
+        super(PredictionStreamer, self).__init__()
+
+
+
+    def createStream(self):
+        
